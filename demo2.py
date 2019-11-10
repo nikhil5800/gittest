d6c8148177afb518f6d4aaa547b96d741abf73df
Nikhil K Singh <Nikhil5.Singh@in.ey.com>
2019-11-10 14:00:18 +0530

    changes

--- demo2.py
+++ demo2.py
@@ -1,19 +1,67 @@
-lst1 = [2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20]
-insert = []
-update = []
-def multi_run_wrapper(arg):
-   return add(arg)
-def add(x):
-    if x % 2:
-        insert.append(x)
-    else:
-        update.append(x)
+import multiprocessing
+from ctypes import c_char_p
+import traceback
+import numpy as np
 
-print("nik")
+def tonumpyarray(shared_array):
+    '''Create numpy array from shared memory.'''
+    try:
+        nparray= np.frombuffer(shared_array,dtype=c_char_p)
+        assert nparray.base is shared_array
+        return nparray
+    except:
+        print(traceback.format_exc())
+
+
+
+def square_list(mylist, result, lst):
+    """
+    function to square a given list
+    """
+    # append squares of mylist to result array
+    try:
+        for idx, num in enumerate(mylist):
+            if num in lst:
+                result[idx] = c_char_p(num.encode('utf-8'))
+
+        # square_sum value
+
+
+    # print result Array
+        print("Result(in process p1): {}".format(result[:]))
+    except:
+        print(traceback.format_exc())
+
+
+
+    # print square_sum Value
+
+
+
 if __name__ == "__main__":
-    from multiprocessing import Pool
-    pool = Pool(4)
-    results = pool.map(multi_run_wrapper,lst1)
+    # input list
+    mylist = ['sku1','sku2','sku3','sku4']
+    lst = ['sku2','sku4']
+    # creating Array of int data type with space for 4 integers
+    result = multiprocessing.Array(c_char_p, len(mylist))
 
-    print("nikhil test")
+    #lst = [2,4]
+    # creating Value of int data type
+    #square_sum = multiprocessing.Value(c_char_p)
+
+    # creating new process
+    p1 = multiprocessing.Process(target=square_list, args=(mylist, result, lst))
+
+    # starting process
+    p1.start()
+
+    # wait until process is finished
+    p1.join()
+    p1.close()
+    # print result array
+
+    print(tonumpyarray(result))
+
+    # print square_sum Value
+    #print("Sum of squares(in main program): {}".format(square_sum.value))
 
--- test.py
+++ test.py
@@ -1,18 +1,36 @@
-from multiprocessing import Pool
-#import workers
-lst1 = [2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20]
-insert = []
-update = []
-def fun(i):
-        if i % 2 == 0:
-            insert.append(i)
-        else:
-            update.append(i)
+from multiprocessing import Process, Manager
 
-if __name__ ==  '__main__':
-    pool = Pool(4)
-    result = pool.map(fun, lst1)
-    print(len(insert))
+def dothing(L, i):  # the managed list `L` passed explicitly.
+    for elems in i:
+        if elems % 2 == 0:
+            L.append(i)
 
 
+    # for j in range(5):
+    #     text = "Process " + str(i) + ", Element " + str(j)
+    #     L.append(text)
 
+L = []
+
+if __name__ == "__main__":
+    with Manager() as manager:
+        L = manager.list()  # <-- can be shared between processes.
+        processes = []
+
+        for i in range(5):
+            p = Process(target=dothing, args=(L,[2,3,4,6,7,8,9,10],))  # Passing the list
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+        L = list(L)
+        print("Within WITH")
+        print(L)
+
+    print("Within IF")
+    print(L)
+
+#print("Outside of IF")
+#print(L)
